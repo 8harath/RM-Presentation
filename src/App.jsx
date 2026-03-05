c83d698446ed827676e1eee4aad91ff295338862
Bharath K <8harath.k@gmail.com>
2026-03-05 22:09:33 +0530

    Align deck flow to faculty-required 10 sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ import Slide4 from './components/slides/Slide4'
 import Slide5 from './components/slides/Slide5'
 import Slide6 from './components/slides/Slide6'
 import Slide7 from './components/slides/Slide7'
-import Slide8 from './components/slides/Slide8'
 import Slide9 from './components/slides/Slide9'
 import Slide10 from './components/slides/Slide10'
 import Slide11 from './components/slides/Slide11'
 
 const slides = [
   Slide1, Slide2, Slide3, Slide4, Slide5, Slide6,
-  Slide7, Slide8, Slide9, Slide10, Slide11
+  Slide7, Slide9, Slide10, Slide11
 ]
 
 function App() {
